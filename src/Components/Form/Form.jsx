f02fa6207f3a29db2c578fe528f3d1fbcf7c6308
Rachel Howell <rac777blue@hotmail.com>
2022-04-11 16:58:27 +0100

    Add local storage to form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,9 +4,18 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+   const saveToLocalStorage = (data) => {
+    localStorage.setItem("Name", data.name);
+    localStorage.setItem("Email", data.email);
+    localStorage.setItem("Telephone", data.telephone);
+    console.log(data);
+  }
+
   const { register, handleSubmit } = useForm();
+
   const onSubmit = (data) => {
-    (console.log(data));
+    saveToLocalStorage(data);
+    routeChange();
   }
 
   let navigate = useNavigate(); 
@@ -22,22 +31,37 @@ const Form = () => {
       >
         <div className="govuk-form-group">
           <label className="govuk-label">Name</label>
-          <input className="govuk-input govuk-input--width-20" type="text" required {...register("nam")} />
+          <input 
+            className="govuk-input govuk-input--width-20" 
+            type="text" 
+            required 
+            {...register("name")} 
+            />
         </div>
 
         <div className="govuk-form-group">
           <label className="govuk-label">Email</label>
-          <div id="event-name-hint" class="govuk-hint">
+          <div id="event-name-hint" className="govuk-hint">
             We'll use this to send you your booking details
           </div>
-          <input className="govuk-input govuk-input--width-20" type="email" required {...register("email")} />
+          <input 
+            className="govuk-input govuk-input--width-20" 
+            type="email" 
+            required 
+            {...register("email")} 
+            />
         </div>
         
         <div className="govuk-form-group">
           <label className="govuk-label">Telephone number</label>
-          <input className="govuk-input govuk-input--width-20" pattern="^\s*\(?(020[7,8]{1}\)?[ ]?[1-9]{1}[0-9{2}[ ]?[0-9]{4})|(0[1-8]{1}[0-9]{3}\)?[ ]?[1-9]{1}[0-9]{2}[ ]?[0-9]{3})\s*$" required {...register("telephone")} />
+          <input 
+            className="govuk-input govuk-input--width-20" 
+            // pattern="^\s*\(?(020[7,8]{1}\)?[ ]?[1-9]{1}[0-9{2}[ ]?[0-9]{4})|(0[1-8]{1}[0-9]{3}\)?[ ]?[1-9]{1}[0-9]{2}[ ]?[0-9]{3})\s*$"
+            required 
+            {...register("telephone")}
+             />
         </div>
-          <input onClick={routeChange}className="govuk-button" type="submit" value="Continue" />
+        <input className="govuk-button" type="submit" value="Continue" />
       </form>
       <div className="govuk-grid-column-one-third"></div>
     </div>
