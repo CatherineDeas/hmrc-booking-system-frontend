59c4cd7bdc3a23144e606bbf3e4649a59c5f2dfa
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-04-11 11:17:20 +0100

    add body to appointments page

--- src/Pages/AppointmentSelection/AppointmentSelection.jsx
+++ src/Pages/AppointmentSelection/AppointmentSelection.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import DefaultLabel from "../../Components/DefaultLabel";
+import BodyContent from "../../Components/BodyContent";
 import styles from "./AppointmentSelection.module.scss";
 
 const AppointmentSelection = () => {
+  const title = "Choose a date"
+  const content = (
+    <p>Select a week and weekday to see the available time slots.</p>
+  )
+
   return (
     <>
-      <DefaultLabel title="Choose a date"/>
-      {/* <BodyContent content={ bodyContent }/> */}
+      <DefaultLabel title="Choose a date" />
+      <BodyContent content={ content } />
     </>
   );
 };
