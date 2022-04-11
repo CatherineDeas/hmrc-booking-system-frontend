b6d33b263ce9405eaf1b8c0513c9e2842fcf5844
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-04-11 14:52:43 +0100

    WIP: add calendar logic

--- src/Components/CalendarTabs/CalendarTabs.jsx
+++ src/Components/CalendarTabs/CalendarTabs.jsx
@@ -3,7 +3,7 @@ import AppointmentTabs from "../AppointmentTabs"
 import styles from "./CalendarTabs.module.scss";
 
 const CalendarTabs = () => {
-  const tabs = [
+  const weeks = [
     {
       id: "test-1",
       label: "test 1",
@@ -15,8 +15,44 @@ const CalendarTabs = () => {
     }
   ]
 
+  // Fetch current date
+  const currentDate = new Date()
+  const currentWeekDay = currentDate.toString().slice(0, 3)
+  const todayIsWeekend = () => { 
+    if (currentWeekDay === "Sat" || currentWeekDay === "Sun") { return true }
+    return false
+  }
+
+  // Identify first week in series
+  const monOfFirstWeek = () => {
+    let firstMonday = currentDate
+
+    if (todayIsWeekend()) {
+      if (currentWeekDay === "Sun") {
+        firstMonday = firstMonday.setDate(firstMonday.getDate() + 1)
+      }
+      if (currentWeekDay === "Sat") {
+        firstMonday = firstMonday.setDate(firstMonday.getDate() + 2)
+      }
+    }
+
+    if (!todayIsWeekend() && currentDate.getDay() != 1) {
+      if (currentDate.getDay() != 0 && currentDate.getDay() != 6) {
+        const todayNumber = currentDate.getDay()
+        firstMonday = firstMonday.setDate(firstMonday - (todayNumber - 1))
+      }
+    }
+
+    return firstMonday
+  }
+
+
+  // Identify all 4 weeks
+
+  // Format week into tab label
+
   return (
-    <AppointmentTabs tabs={ tabs } />
+    <AppointmentTabs tabs={ weeks } />
   );
 };
 
